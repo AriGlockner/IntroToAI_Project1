22b378640a2cef05c0584cb3f3750b368a5c5c88
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-09-11 15:56:20 -0400

    Solved move method

--- EightPuzzle/src/EightPuzzle.java
+++ EightPuzzle/src/EightPuzzle.java
@@ -3,8 +3,7 @@ import java.util.*;
 
 public class EightPuzzle
 {
-
-	private char[][] state = new char[3][];
+	private String state;
 
 	public EightPuzzle(String file)
 	{
@@ -26,13 +25,12 @@ public class EightPuzzle
 	 */
 	public void setState(String newState)
 	{
-		state[0] = new char[] {newState.charAt(0), newState.charAt(1), newState.charAt(2)};
-		state[1] = new char[] {newState.charAt(4), newState.charAt(5), newState.charAt(6)};
-		state[2] = new char[] {newState.charAt(8), newState.charAt(9), newState.charAt(10)};
+		state = newState;
 	}
 
 	public void printState()
 	{
+		/*
 		StringBuilder stringBuilder = new StringBuilder();
 		for (char[] c1 : state)
 		{
@@ -41,19 +39,88 @@ public class EightPuzzle
 			stringBuilder.append(" ");
 		}
 		System.out.println(stringBuilder.substring(0, 11));
+		 */
+		System.out.println(state);
 	}
 
 	//TODO: Remove when done
 	@Override
 	public String toString()
 	{
-		return Arrays.toString(state[0]) + "\n" + Arrays.toString(state[1]) + "\n" + Arrays.toString(state[2]);
+		return state.replaceAll(" ", "\n");
+	}
+
+	public enum Direction {
+		up, down, left, right
+	}
+
+	public boolean move(Direction direction)
+	{
+		// where blank is at in puzzle
+		int blankIndex = state.indexOf('b');
+
+		// Unable to move left
+		if (direction.equals(Direction.left) && blankIndex % 4 == 0)
+			return false;
+		// Unable to move right
+		if (direction.equals(Direction.right) && blankIndex % 4 == 2)
+			return false;
+		// Unable to move up
+		if (direction.equals(Direction.up) && blankIndex < 3)
+			return false;
+		// Unable to move down
+		if (direction.equals(Direction.down) && blankIndex > 7)
+			return false;
+
+		// Swap left
+		if (direction.equals(Direction.left))
+			swap(blankIndex, blankIndex - 1);
+
+		// Swap right
+		else if (direction.equals(Direction.right))
+			swap(blankIndex, blankIndex + 1);
+
+		// Swap up
+		else if (direction.equals(Direction.up))
+			swap(blankIndex, blankIndex - 4);
+
+		// Swap down
+		else
+			swap(blankIndex, blankIndex + 4);
+
+		return true;
+	}
+
+	/**
+	 * Helper method that preforms swapping function for move method
+	 *
+ 	 * @param blankIndex index where the blank tile is at
+	 * @param newBlankIndex index where blank index is going to be
+	 */
+	private void swap(int blankIndex, int newBlankIndex)
+	{
+		// swap
+		char[] stateArray = state.toCharArray();
+		stateArray[blankIndex] = stateArray[newBlankIndex];
+		stateArray[newBlankIndex] = 'b';
+
+		// convert array to String
+		StringBuilder stringBuilder = new StringBuilder();
+		for (char c : stateArray)
+			stringBuilder.append(c);
+		state = stringBuilder.substring(0, 11);
 	}
 
 	public static void main(String[] args)
 	{
 		EightPuzzle puzzle = new EightPuzzle("C:\\Users\\ari\\git\\CSDS391-P1\\out\\production\\EightPuzzle\\EightPuzzle1.txt");
-		puzzle.printState();
+		//puzzle.printState();
 		System.out.println(puzzle);
+		System.out.println(puzzle.move(Direction.left) + ":\n" + puzzle);
+		System.out.println(puzzle.move(Direction.right) + ":\n" + puzzle);
+		System.out.println(puzzle.move(Direction.left) + ":\n" + puzzle);
+		System.out.println(puzzle.move(Direction.up) + ":\n" + puzzle);
+		System.out.println(puzzle.move(Direction.up) + ":\n" + puzzle);
+		System.out.println(puzzle.move(Direction.down) + ":\n" + puzzle);
 	}
 }
